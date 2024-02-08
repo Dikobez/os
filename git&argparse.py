d8507cb5ac622e3f9d47f14fe4cd124c91016613
Inv <dikobez@gmail.com>
2024-02-08 18:05:08 +0400

    api ya

---
+++ git&argparse.py
@@ -0,0 +1,71 @@
+import requests
+
+serv_add = input()
+port_serv = input()
+a, b = input(), input()
+params = {"a": a, "b": b}
+response = requests.get(f"{serv_add}:{port_serv}", params=params)
+if response:
+    response = response.json()
+    print(" ".join(map(str, sorted(response["result"]))))
+    print(response["check"])
+
+import pygame, requests, sys, os
+
+
+# Создайте оконное приложение, отображающее карту по координатам и в масштабе, который задаётся программно.
+
+class MapParams(object):
+    def __init__(self):
+        self.lat = 61.665279  # Координаты центра карты на старте. Задал координаты университета
+        self.lon = 50.813492
+        self.zoom = 16  # Масштаб карты на старте. Изменяется от 1 до 19
+        self.type = "map"  # Другие значения "sat", "sat,skl"
+
+    # Преобразование координат в параметр ll, требуется без пробелов, через запятую и без скобок
+    def ll(self):
+        return str(self.lon) + "," + str(self.lat)
+
+
+# Создание карты с соответствующими параметрами.
+def load_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запись полученного изображения в файл.
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+    return map_file
+
+
+def main():
+    # Инициализируем pygame
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    mp = MapParams()
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:  # Выход из программы
+            break
+        # Создаем файл
+        map_file = load_map(mp)
+        # Рисуем картинку, загружаемую из только что созданного файла.
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+    pygame.quit()
+    # Удаляем файл с изображением.
+    os.remove(map_file)
+
+
+if __name__ == "__main__":
+    main()
